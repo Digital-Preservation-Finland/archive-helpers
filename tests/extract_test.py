caebb7c43d79d6ca9eaf7b0f2d3309d938b3d71c
Thien Tran <thien.tran@csc.fi>
2020-02-14 14:21:11 +0200

    SAPA-338 Resolve pylint warning

--- tests/extract_test.py
+++ tests/extract_test.py
@@ -307,30 +307,22 @@ def test_extract_success(archive, use_stream, tmpdir):
 
 
 @pytest.mark.parametrize(
-    "archive,dirs,files",
-    [
-        ("tests/data/windows_zip.zip", (
-                "windows_zip",
-                "windows_zip/directory"
-        ), (
-                 "windows_zip/directory/file.txt",
-                 "windows_zip/directory/file2.txt"
-         )),
-        ("tests/data/windows_zip_symlinks.zip", (
-                "windows_zip_symlinks",
-                "windows_zip_symlinks/directory",
-                "windows_zip_symlinks/directory_junction_link",
-                "windows_zip_symlinks/soft_link"
-        ), (
-                 "windows_zip_symlinks/file.txt",
-                 "windows_zip_symlinks/symlink",
-                 "windows_zip_symlinks/hard_link",
-                 "windows_zip_symlinks/directory/file.txt",
-                 "windows_zip_symlinks/directory/file2.txt",
-                 "windows_zip_symlinks/soft_link/file.txt",
-                 "windows_zip_symlinks/soft_link/file2.txt"
-         )),
-    ],
+    ('archive', 'dirs', 'files'),
+    [("tests/data/windows_zip.zip",
+      ("windows_zip", "windows_zip/directory"),
+      ("windows_zip/directory/file.txt", "windows_zip/directory/file2.txt")),
+     ("tests/data/windows_zip_symlinks.zip",
+      ("windows_zip_symlinks",
+       "windows_zip_symlinks/directory",
+       "windows_zip_symlinks/directory_junction_link",
+       "windows_zip_symlinks/soft_link"),
+      ("windows_zip_symlinks/file.txt",
+       "windows_zip_symlinks/symlink",
+       "windows_zip_symlinks/hard_link",
+       "windows_zip_symlinks/directory/file.txt",
+       "windows_zip_symlinks/directory/file2.txt",
+       "windows_zip_symlinks/soft_link/file.txt",
+       "windows_zip_symlinks/soft_link/file2.txt"))],
     ids=["files", "links"]
 )
 def test_extract_zip_windows(archive, dirs, files, tmpdir):
